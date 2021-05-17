4a95ae7f6a5d7ce1edee8303e1ec34b141fa16fc
ipadorusa <ipadorusa@gmail.com>
2021-05-17 15:00:50 +0900

    react: 20210517  study

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, {useState} from 'react'
+import Profile from './Profile';
 import Clock from './study/Clock'
+import WidthPrinter from './study/WidthPrinter'
 
 function App() {
-  return (
+  const [count, setCount] = useState(0);
+  return ( 
     <div>
+      <Profile />
       <Clock />
+      <div>{count === 0 && <WidthPrinter />}</div>
+      <button type="button" onClick={() => setCount(prev => prev + 1)}>증가</button>
     </div>
   )
 }
