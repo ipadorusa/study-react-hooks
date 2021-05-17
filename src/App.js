48ea623add67d3679fad87c4efdd404a9668f182
ipadorusa <ipadorusa@gmail.com>
2021-05-17 09:19:43 +0900

    ^^

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react'
+import Clock from './study/Clock'
 
 function App() {
-  const [state, setState] = useState({ name: '', age: 0 });
   return (
     <div>
-      <p>{`name is ${state.name}`}</p>
-      <p>{`age is ${state.age}`}</p>
-      <input type='text' value={state.name} onChange={e => setState({ ...state, name: e.target.value })} />
-      <input type='text' value={state.age} onChange={e => setState({ ...state, age: e.target.value })} />
+      <Clock />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
