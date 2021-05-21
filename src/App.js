6eca8ccb127e6b1cf05f358e225c104a0acf93ff
ipadorusa <ipadorusa@gmail.com>
2021-05-22 01:26:32 +0900

    study:context

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import LazyExamComponent from './study/lazy/LazyExamComponent'
+import Example_context from './study/context/Example_context'
 import InputSample from './study/useRef/InputSample'
 
 function App() {
   return (
     <div>
       <InputSample />
-      <LazyExamComponent />
+      <Example_context />
     </div>
   )
 }
