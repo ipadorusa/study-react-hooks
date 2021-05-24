a7d06a00718fdbea992cea888bf4ed97e08a5084
ipadorusa <ipadorusa@gmail.com>
2021-05-24 13:38:47 +0900

    study:React useEffect

--- src/App.js
+++ src/App.js
@@ -1,14 +1,64 @@
-import React from 'react'
-import Example_context2 from './study/context/Example_context2'
-import InputSample from './study/useRef/InputSample'
+import React, { useRef, useState } from 'react';
+import UserList from './study/useEffect/UserList';
+import CreateUser from './study/useEffect/CreateUser';
 
 function App() {
+  const [inputs, setInputs] = useState({
+    username: '',
+    email: ''
+  });
+  const { username, email } = inputs;
+  const onChange = e => {
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value
+    });
+  };
+  const [users, setUsers] = useState([
+    {
+      id: 1,
+      username: 'velopert',
+      email: 'public.velopert@gmail.com'
+    },
+    {
+      id: 2,
+      username: 'tester',
+      email: 'tester@example.com'
+    },
+    {
+      id: 3,
+      username: 'liz',
+      email: 'liz@example.com'
+    }
+  ]);
+
+  const nextId = useRef(4);
+  const onCreate = () => {
+    const user = {
+      id: nextId.current,
+      username,
+      email
+    };
+    setUsers(users.concat(user));
+
+    setInputs({
+      username: '',
+      email: ''
+    });
+    nextId.current += 1;
+  };
   return (
-    <div>
-      <InputSample />
-      <Example_context2 />
-    </div>
-  )
+    <>
+      <CreateUser
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}
+      />
+      <UserList users={users} />
+    </>
+  );
 }
 
-export default App
+export default App;
